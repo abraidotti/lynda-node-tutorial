7a40385f97ccd85b16907e1aa70d9a464c0f13c2
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-01-16 15:55:18 -0500

    make a way into the chat app

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000);
+app.use(express.static(__dirname));
+var server = app.listen(3000, () => {
+  console.log('server is listening on port', server.address().port)
+});
