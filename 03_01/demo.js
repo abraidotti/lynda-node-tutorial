4ff3b4e5ad2c274fbb957aaa7c0dfc39878820ab
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-01-16 14:43:58 -0500

    add files

---
+++ 03_01/demo.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var data = require('./data.json')
+
+console.log(data.name)
+
+fs.readFile('./data.json', 'utf-8', (err,data) => {
+    var data = JSON.parse(data)
+    console.log(data.name)
+})
+
+var fs = require('fs');
+fs.readFile('./data.json');
