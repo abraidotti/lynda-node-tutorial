b1eb9bfa84369fc2957b4b8aa5a132fe9f7045fe
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-01-17 12:52:32 -0500

    add two tests

--- chat-app/spec/server.spec.js
+++ chat-app/spec/server.spec.js
@@ -9,7 +9,14 @@ describe('calc', () => {
 describe('get messages', () => {
   it('should return 200 OK', () => {
     request.get('http://localhost:3000', (err, res) => {
-      console.log(res);
+      expect(res.statusCode).toEqual(200);
+      done()
+    })
+  })
+  it('should return a list that is not empty', () => {
+    request.get('http://localhost:3000', (err, res) => {
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+      done()
     })
   })
 })
